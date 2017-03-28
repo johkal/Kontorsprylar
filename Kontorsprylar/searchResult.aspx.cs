6d6ae2e10b5024c0081a8f859d6af1b03ab7d046
Administrator <Administrator@DONATELLO>
2017-03-28 19:48:54 +0200

    Söksidan

--- Kontorsprylar/searchResult.aspx.cs
+++ Kontorsprylar/searchResult.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,14 +10,31 @@ namespace Kontorsprylar
 {
     public partial class searchResult : System.Web.UI.Page
     {
-        //protected void Page_Load(object sender, EventArgs e)
-        //{
-        //    searchResultDiv.InnerHtml = "";
+        public NameValueCollection QueryString { get; }
 
-        //    foreach (var products in matchingProducts)
-        //    {
-        //        searchResultDiv.InnerHtml += products.....
-        //    }
-        //}
+        protected void page_load(object sender, EventArgs E)
+        {
+            List<SQLKAB.Product> allProducts = SQLKAB.SQL.GetAllProducts();
+            List<SQLKAB.Product> matchingProducts = new List<SQLKAB.Product>();
+            List<SQLKAB.Category> allCategories = SQLKAB.SQL.GetAllCategories();
+            List<SQLKAB.Category> matchingCategories = new List<SQLKAB.Category>();
+
+            string input = Request.QueryString["searchString"];
+
+            foreach (var prod in allProducts)
+            {
+                if (prod.Name.ToLower().StartsWith(input.ToLower()))
+                {
+                    matchingProducts.Add(prod);
+                }
+            }
+            
+            searchResultDiv.InnerHtml = "";
+
+            foreach (var product in matchingProducts)
+            {
+                searchResultDiv.InnerHtml += "<h2><a href='details.aspx?PID=" + product.ID + "'>" + product.Name.ToString() + "</a></h2>\n";
+            }
+        }
     }
 }
