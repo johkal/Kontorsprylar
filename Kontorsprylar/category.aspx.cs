965e1cfa9b9aefcae447f6da9d7703cfe9a77941
Jörgen Eliasson <jorgen.eliasson@gmail.com>
2017-03-28 10:30:16 +0200

    Added category.aspx

--- Kontorsprylar/category.aspx.cs
+++ Kontorsprylar/category.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Sql;
+using SQLKAB;
 
 namespace Kontorsprylar
 {
@@ -11,7 +13,23 @@ namespace Kontorsprylar
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                int catID = Convert.ToInt32(Request.QueryString["Id"]);
+                categoryID.InnerHtml = "<h1>" + SQL.FindCategory(catID.ToString()).Name + "</h1>\n";
+                
+                List<Product> produktlista = SQL.GetProductsInCategory(catID);
+                foreach (var prod in produktlista)
+                {
+                    categoriesDiv.InnerHtml += "<h2>" + prod.Name.ToString() + "</h2>\n";
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
     }
 }
