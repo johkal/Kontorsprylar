962fcdc2b9702f8cc14ff0b5c9a711580facfcb7
Jörgen Eliasson <jorgen.eliasson@gmail.com>
2017-03-29 11:18:02 +0200

    Awesome varukorg

--- Kontorsprylar/cart.aspx
+++ Kontorsprylar/cart.aspx
@@ -3,4 +3,6 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="main" runat="server">
     <h1>Varukorg</h1>
+    <div id="showCart" runat="server"></div>
+    <asp:Button Text="Gå till kassan" runat="server" CssClass="button" />
 </asp:Content>
--- Kontorsprylar/cart.aspx.cs
+++ Kontorsprylar/cart.aspx.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SQLKAB;
 
 namespace Kontorsprylar
 {
     public partial class cart : System.Web.UI.Page
     {
+        static List<Cart> productCart = new List<Cart>();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["Cart"] != null)
+            {
+                productCart = (List<Cart>)Session["Cart"];
+                showCart.InnerHtml = "<ul>\n";
+                int summa = 0;
+                foreach (var korg in productCart)
+                {
+                showCart.InnerHtml += "<li>" + korg.Product.Name.ToString() + ", antal: " + korg.NumberOfProducts + " st á pris: " + korg.Product.NetPrice +  " kr, delsumma: " + Convert.ToInt32(korg.NumberOfProducts) * Convert.ToInt32(korg.Product.NetPrice) + " kr</li>\n";
+                    summa = summa + (Convert.ToInt32(korg.NumberOfProducts) * Convert.ToInt32(korg.Product.NetPrice));
+                }
+                showCart.InnerHtml += "</ul><br>\n";
+                showCart.InnerHtml += "<h3>Summa: " + summa + " kr</h3>";
+            }
+            else
+                showCart.InnerText = "Varukorgen är tom.";
         }
     }
 }
